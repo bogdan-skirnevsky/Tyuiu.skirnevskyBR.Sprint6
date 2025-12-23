d33476ce1f6c0fbba85fa406778b351a402f4f1f
Богдан Скирневский <tyuiu.skirnevskybr@outlook.com>
2025-12-24 01:07:30 +0500

    Task0

---
+++ Tyuiu.skirnevskyBR.Sprint6.Task0.V16/Form1.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.skirnevskyBR.Sprint6.Task0.V16
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
