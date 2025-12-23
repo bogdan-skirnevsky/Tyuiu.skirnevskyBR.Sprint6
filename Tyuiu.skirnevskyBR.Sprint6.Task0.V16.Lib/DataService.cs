eaed63ab883900e815f6e7e580a09f9974270cd3
Богдан Скирневский <tyuiu.skirnevskybr@outlook.com>
2025-12-24 01:01:35 +0500

    Task0

--- Tyuiu.skirnevskyBR.Sprint6.Task0.V16.Lib/DataService.cs
+++ Tyuiu.skirnevskyBR.Sprint6.Task0.V16.Lib/DataService.cs
@@ -1,8 +1,12 @@
 ﻿using tyuiu.cources.programming.interfaces.Sprint6;
 namespace Tyuiu.skirnevskyBR.Sprint6.Task0.V16.Lib
 {
-    public class DataService
+    public class DataService : ISprint6Task0V16
     {
-
+        public double Calculate(int x)
+        {
+            double result = (1 + Math.Pow(x, 3)) / Math.Pow(x, 2);
+            return Math.Round(result, 3);
+        }
     }
 }
--- Tyuiu.skirnevskyBR.Sprint6.Task0.V16.Test/DataServiceTest.cs
+++ Tyuiu.skirnevskyBR.Sprint6.Task0.V16.Test/DataServiceTest.cs
@@ -5,8 +5,15 @@ namespace Tyuiu.skirnevskyBR.Sprint6.Task0.V16.Test
     public sealed class DataServiceTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ValidCalculate()
         {
+            DataService ds = new DataService();
+            int x = 3;
+
+            double wait = 3.111;
+            double res = ds.Calculate(x);
+
+            Assert.AreEqual(wait, res);
         }
     }
 }
